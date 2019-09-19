17be8a7e5efd7935a64fda4124d467ae5b040903
Bishal <ibishalgoswami@gmail.com>
2019-09-19 12:30:09 +0530

    some changes have been done

--- alarm.py
+++ alarm.py
@@ -13,55 +13,51 @@ res = inptime.split("-")
 inptext = input("What do you want to be remained?")
 # start=int(time.time())+inptime
 
-if len(res) == 3:
-    H = int(res[0])
-    M = int(res[1])
-    S = int(res[2])
-    if H > 24:
-        try:
-            raise inperror("Please Provide the correct time")
-        except inperror as e:
-            print(e.msg)
+try:
+    if len(res) == 3:
+        H = int(res[0])
+        M = int(res[1])
+        S = int(res[2])
+        if H > 24:
+            try:
+                raise inperror("Please Provide the correct time")
+            except inperror as e:
+                print(e.msg)
 
-    elif M > 60:
-        try:
-            raise inperror("Please Provide the correct time")
-        except inperror as e:
-            print(e.msg)
+        elif M > 60:
+            try:
+                raise inperror("Please Provide the correct time")
+            except inperror as e:
+                print(e.msg)
+        else:
+            while True:
+                if int(time.localtime().tm_hour) == H and int(time.localtime().tm_min) == M and int(
+                        time.localtime().tm_sec) == S:
+                    notifier = ToastNotifier()
+                    notifier.show_toast("Hello", inptext, duration=12)
+                    break
+
     else:
-        while True:
-            if int(time.localtime().tm_hour) == H and int(time.localtime().tm_min) == M and int(
-                    time.localtime().tm_sec) == S:
-                notifier = ToastNotifier()
-                notifier.show_toast("Hello", inptext, duration=12)
-                break
+        H = int(res[0])
+        M = int(res[1])
+        S = None
+        if H > 24:
+            try:
+                raise inperror("Please Provide the correct time")
+            except inperror as e:
+                print(e.msg)
 
-else:
-    H = int(res[0])
-    M = int(res[1])
-    S = None
-    if H > 24:
-        try:
-            raise inperror("Please Provide the correct time")
-        except inperror as e:
-            print(e.msg)
+        elif M > 60:
+            try:
+                raise inperror("Please Provide the correct time")
+            except inperror as e:
+                print(e.msg)
+        else:
+            while True:
+                if int(time.localtime().tm_hour) == H and int(time.localtime().tm_min) == M:
+                    notifier = ToastNotifier()
+                    notifier.show_toast("Your Alarm", inptext, duration=12)
+                    break
 
-    elif M > 60:
-        try:
-            raise inperror("Please Provide the correct time")
-        except inperror as e:
-            print(e.msg)
-    else:
-        while True:
-            if int(time.localtime().tm_hour) == H and int(time.localtime().tm_min) == M:
-                notifier = ToastNotifier()
-                notifier.show_toast("Hello", inptext, duration=12)
-                break
-
-
-
-
-
-
-
-
+except:
+    print("Provide the time in the correct input format")
